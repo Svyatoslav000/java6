f679c8cf593134c8e16507e1bd94bdd9b038f777
Svyatoslav000 <90640892+Svyatoslav000@users.noreply.github.com>
2022-09-23 15:37:01 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,51 @@
+public class Main
+{
+    public static void main(String[] args) {
+        System.out.println("Hello World");//первое задание
+
+        int x=1;//второе задание
+        System.out.println(x);
+
+        int y=1;//3 задание
+        y++;
+        System.out.println(y);
+        int z=1;
+        z=z+1;
+        System.out.println(z);
+
+        int a=5;//4 задача
+        int b=6;
+        int c=0;
+        c = a;
+        a = b;
+        b = c;
+        System.out.println(a);
+        System.out.println(b);
+
+        double a1=3;//5 задача
+        double b1=4;
+        double c1=0;
+        c1=Math.sqrt(a1*a1 + b1*b1);
+        System.out.println(c1);
+
+        int e = 133;//6 задача
+        e%=10;
+        System.out.println(e);
+
+        int g=157;//7 задача
+        g%=100;
+        g/=10;
+        System.out.println(g);
+
+        int h=78;//8 задача
+        h/=10;
+        System.out.println(h);
+
+
+
+        }
+        static void meth1(int ch1){
+            int razn=ch1-21;
+
+}
+    }
