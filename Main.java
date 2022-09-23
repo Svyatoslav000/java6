83d26e68a1dec4a8f01269092f09b8ff0daf2b91
Svyatoslav000 <90640892+Svyatoslav000@users.noreply.github.com>
2022-09-23 16:40:31 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -42,10 +42,41 @@ public class Main
         System.out.println(h);
 
 
+        int ch1=56;//9 zadanie
+        meth1(ch1);
+
+
+        int ch2=20;//10 zadanie
+        int ch3=24;
+        meth2(ch2, ch3);
+
+
+        int ch4=3;//11 zadanie
+        int ch5=12;
+        meth3(ch4, ch5);
+
+        double x1=9;//12 zadanie
+        double y1=-1;
+        double x2=2;
+        double y2=-25;
+        meth4(x1,y1,x2,y2);
 
         }
         static void meth1(int ch1){
             int razn=ch1-21;
+            System.out.println(razn);
+        }
+        static void meth2(int ch1, int ch2){
+            int srAriph=(ch1+ch2)/2;
+            System.out.println(srAriph);
+        }
+        static void meth3(int ch1, int ch2){
+            double srGeom=Math.sqrt(ch1*ch2);
+            System.out.println(srGeom);
+        }
+        static void meth4(double x1, double y1, double x2, double y2){
+            double rast=Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+            System.out.println(rast);
+        }
 
-}
     }
